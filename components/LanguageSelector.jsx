223f0a334f09bf3b47f656b8f2b5a30ebe758ff9
sumant-k <1604014@ritindia.edu>
2021-09-23 22:30:34 +0530

    header modified

---
+++ components/LanguageSelector.jsx
@@ -0,0 +1,10 @@
+export default function LanguageSelector() {
+  return (
+    <div className={"language-selector"}>
+      <select name="header-language-select" id="header-language-select">
+        <option value="english">English</option>
+        <option value="hindi">Hindi</option>
+      </select>
+    </div>
+  );
+}
